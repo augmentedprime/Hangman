ea5567357e8f9a0707e348d906cc402388b7bdbd
Austin Myers <Austin Myers>
2016-09-08 23:33:05 -0500

    Created LetterMatcher Test and wrote litte stub of LetterMatcher method

---
+++ src/hangman/LetterMatcherTest.java
@@ -0,0 +1,14 @@
+package hangman;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LetterMatcherTest {
+
+	@Test
+	public void emptyString() {
+		String expected = "";
+	}
+
+}
