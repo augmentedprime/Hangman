55508a232e1c90047f33f97ad986cdda2738825e
Austin Myers <Austin Myers>
2016-09-09 00:44:28 -0500

    Implemented LetterMatcher (with tests)

--- src/hangman/LetterMatcher.java
+++ src/hangman/LetterMatcher.java
@@ -1,5 +1,15 @@
 package hangman;
 
 public class LetterMatcher {
-
+	
+	public boolean match(String givenLetter, String givenWord){
+		
+		
+		for (int i = 0; i<givenWord.length(); i++){
+			if (givenLetter.equals(givenWord.substring(i, i+1))){
+				return true;
+			}
+		}
+		return false;
+	}
 }
--- src/hangman/LetterMatcherTest.java
+++ src/hangman/LetterMatcherTest.java
@@ -6,9 +6,43 @@ import org.junit.Test;
 
 public class LetterMatcherTest {
 
+	LetterMatcher gay = new LetterMatcher();
+	
+	
 	@Test
-	public void emptyString() {
-		String expected = "";
+	public void letterInGivenWord() {
+		boolean test = gay.match("b", "bitchhole");
+		assertTrue(test);
+	}
+	
+	@Test
+	public void lastLetterInGivenWord() {
+		boolean test = gay.match("e", "bitchhole");
+		assertTrue(test);
+	}
+	
+	@Test
+	public void letterNotInGivenWord() {
+		boolean test = gay.match("b", "itchhole");
+		assertFalse(test);
+	}
+	
+	@Test
+	public void moreThanaSingleLetterGiven() {
+		boolean test = gay.match("bi", "bitchhole");
+		assertFalse(test);
+	}
+	
+	@Test
+	public void twoLettersAtEndOfWord(){
+		boolean test = gay.match("le", "bitchhole");
+		assertFalse(test);
+	}
+	
+	@Test
+	public void letterGivenEmptyString() {
+		boolean test = gay.match("", "Matt's Dick");
+		assertFalse(test);
 	}
 
 }
