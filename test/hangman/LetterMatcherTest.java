79b355627baab3ea3712d43d354f1d8b008c335c
Austin Myers <Austin Myers>
2016-09-14 14:53:57 -0500

    .

--- test/hangman/LetterMatcherTest.java
+++ test/hangman/LetterMatcherTest.java
@@ -6,42 +6,42 @@ import org.junit.Test;
 
 public class LetterMatcherTest {
 
-	LetterMatcher gay = new LetterMatcher();
+	LetterMatcher letterMatcher = new LetterMatcher();
 	
 	
 	@Test
 	public void letterInGivenWord() {
-		boolean test = gay.match("b", "bitchhole");
+		boolean test = letterMatcher.match("b", "bitchhole");
 		assertTrue(test);
 	}
 	
 	@Test
 	public void lastLetterInGivenWord() {
-		boolean test = gay.match("e", "bitchhole");
+		boolean test = letterMatcher.match("e", "bitchhole");
 		assertTrue(test);
 	}
 	
 	@Test
 	public void letterNotInGivenWord() {
-		boolean test = gay.match("b", "itchhole");
+		boolean test = letterMatcher.match("b", "itchhole");
 		assertFalse(test);
 	}
 	
 	@Test
 	public void moreThanaSingleLetterGiven() {
-		boolean test = gay.match("bi", "bitchhole");
+		boolean test = letterMatcher.match("bi", "bitchhole");
 		assertFalse(test);
 	}
 	
 	@Test
 	public void twoLettersAtEndOfWord(){
-		boolean test = gay.match("le", "bitchhole");
+		boolean test = letterMatcher.match("le", "bitchhole");
 		assertFalse(test);
 	}
 	
 	@Test
 	public void letterGivenEmptyString() {
-		boolean test = gay.match("", "Matt's Dick");
+		boolean test = letterMatcher.match("", "Matt's Dick");
 		assertFalse(test);
 	}
 }
