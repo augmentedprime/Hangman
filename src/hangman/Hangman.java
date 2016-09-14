31abac1a341fed5385f612d10bf8229757c22920
Austin Myers <Austin Myers>
2016-09-14 16:26:21 -0500

    No longer double prints hangman when playing

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -59,7 +59,6 @@ public class Hangman {
 		break;
 		default : throw new RuntimeException("Number too big (like Matt's dick(dick))");
 		}
-		System.out.println(expected);
 		return expected;
 	}
 }
